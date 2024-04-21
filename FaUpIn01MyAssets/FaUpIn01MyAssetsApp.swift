640dccff35737bfa8fe9cba644f859b63d4d1431
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-04-21 23:11:59 +0900

    Initial Commit

---
+++ FaUpIn01MyAssets/FaUpIn01MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaUpIn01MyAssetsApp.swift
+//  FaUpIn01MyAssets
+//
+//  Created by joe on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FaUpIn01MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
